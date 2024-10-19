b86be7ddeaff3acd7714db22dc6faab9c5b36709
Aleksei Ivanov <contact@aleksei.dev>
2024-10-19 20:24:32 +0300

    Update .env.example and src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import OkoCRM from "./okocrm-api/okocrm.js";
-import Keyv from "keyv";
-import { KeyvFile } from "keyv-file";
 import ms from "ms";
 import dotenv from "dotenv";
 import { remember } from "./services/cache.js";
 import Debug from "debug";
+import { Lead, Pipeline } from "./okocrm-api/types.js";
 
 dotenv.config();
 
@@ -14,36 +13,48 @@ const api = new OkoCRM({
     apiKey: process.env.OKOCRM_API_KEY || "",
 });
 
-const leads = await api.leads.getLeads();
-
 const pipelines = await remember(
     "pipelines",
     async () => await api.pipelines.getPipelines(),
     ms("1m")
 );
 
-console.log(pipelines);
+const stages = await (async () => {
+    const allStages = [];
 
-// for (const pipeline of pipelines) {
-//     console.log(pipeline, {
-//         stages: await api.pipelines.getPipelineStages(pipeline.id),
-//     });
-// }
+    for (const pipeline of pipelines) {
+        const stages = await remember(
+            `pipelines:${pipeline.id}:stages`,
+            async () => await api.pipelines.getPipelineStages(pipeline.id),
+            ms("1m")
+        );
 
-// const getPipelinesByName = async (names: string[]) => {
-//     const pipelines = await api.pipelines.getPipelines();
+        allStages.push(...stages);
+    }
 
-//     return pipelines.filter((pipeline) => {
-//         return names
-//             .map((s) => s.toLocaleLowerCase().trim())
-//             .includes(pipeline.name.toLocaleLowerCase().trim());
-//     });
+    return allStages;
+})();
+
+const activeStagesNames =
+    process.env.EXPORT_STAGES?.split(",").map((s) =>
+        s.toLocaleLowerCase().trim()
+    ) || [];
+
+const activeStages = stages.filter((stage) => {
+    return activeStagesNames.includes(stage.name.toLocaleLowerCase().trim());
+});
+
+// const getPipelineById = (id: number) => {
+//     return pipelines.find((p: Pipeline) => p.id === id);
 // };
 
-// const pipelines = await getPipelinesByName(["первая покупка", "Пролонгация"]);
+const leads = await api.leads.getLeads();
 
-// for (const pipeline of pipelines) {
-//     console.log(pipeline, {
-//         stages: await api.pipelines.getPipelineStages(pipeline.id),
-//     });
-// }
+const leadsToExport = leads.filter((lead: Lead) => {
+    return activeStages.map((stage) => stage.id).includes(lead.stages_id);
+});
+
+for (const lead of leadsToExport) {
+    const { id, name } = lead;
+    console.log({ id, name });
+}
